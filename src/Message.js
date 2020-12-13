ff98a79b9e275f2e02a99682f148ef1e9cf4d9fb
Rupa <rupa211984@gmail.com>
2020-12-12 23:51:59 -0800

    Format data and time in an okayish way

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,13 @@
 function Message({ msg }) {
+  const formatDateTime = () => {
+    const dateTime = msg.created_at.slice(0, 16).split("T");
+
+    return `${dateTime[0]} at ${dateTime[1]}`;
+  };
+
   return (
     <li>
-      <span className="username">{msg.username}</span> meowed: "{msg.msg}" on {msg.created_at}
+      <span className="username">{msg.username}</span> meowed: "{msg.msg}" on {formatDateTime()}
     </li>
   )
 }
